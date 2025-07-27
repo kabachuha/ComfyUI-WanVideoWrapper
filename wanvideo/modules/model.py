7977951511ae2490f72164eb44557046ce7e779d
kabachuha <artemkhrapov2001@yandex.ru>
2025-07-27 22:10:45 +0300

    implement sito token compression

--- wanvideo/modules/model.py
+++ wanvideo/modules/model.py
@@ -568,6 +568,170 @@ WAN_CROSSATTENTION_CLASSES = {
     'i2v_cross_attn': WanI2VCrossAttention,
 }
 
+def do_nothing_s(x, mode=None):
+    return x
+
+def find_patch_max_indices(tensor, st, sx, sy, t, x, y):  # tensor: (B,N)
+    b, N = tensor.size()
+    tensor = tensor.view(b, t, x, y)
+    t_patches = t // st
+    h_patches = y // sy
+    w_patches = x // sx
+    tensor = tensor[:, :t_patches * st, :h_patches * sy, :w_patches * sx]
+    tensor_reshaped = tensor.view(b, t_patches, st, h_patches, sy, w_patches, sx)
+    tensor_reshaped = tensor_reshaped.permute(0, 1, 3, 5, 2, 4, 6).contiguous()
+    tensor_reshaped = tensor_reshaped.view(b, t_patches, h_patches, w_patches, st * sy * sx)
+    _, max_indices = tensor_reshaped.max(dim=-1, keepdim=True)
+    return max_indices
+    
+def duplicate_half_tensor(tensor):
+    B, _, _,_ = tensor.shape
+    if B % 2 != 0:
+        raise ValueError("B must be even for this operation")
+    first_half = tensor[:B // 2]
+    tensor[B // 2:] = first_half
+    return tensor
+
+@torch.compiler.disable
+def prune_and_recover_tokens(metric: torch.Tensor,
+                                 num_prune: int,
+                                 t: int,
+                                 w: int,
+                                 h: int,
+                                 st: int,
+                                 sx: int,
+                                 sy: int,
+                                 sim_beta: float,
+                                 noise_alpha: float,
+                                 current_timestep: int
+                                 ):
+    B, N, C = metric.shape
+    if num_prune <= 0:  # 如果r<0, 什么也不做
+        return do_nothing_s, do_nothing_s
+    #gather = mps_gather_workaround if metric.device.type == "mps" else torch.gather
+    gather = torch.gather
+    with torch.no_grad():
+        metric = metric.to(torch.float16)
+        metric = torch.nn.functional.normalize(metric, p=2, dim=-1)
+        consine_graph = torch.matmul(metric, metric.transpose(-1, -2))
+        # my_norm = metric.norm(dim=-1, keepdim=True)
+        # my_norm = my_norm.to(torch.float16)
+        # metric = metric / my_norm
+        # consine_graph = metric @ metric.transpose(-1, -2)
+        hst, hsy, wsx = t // st, h // sy, w // sx
+        # ##############################################################
+        # Method 1:   Select the highest score based on (sim_beta * SimScore + noise_alpha * Noise) within each patch.
+        dst_score = consine_graph.sum(-1)
+        noise_score= torch.randn(B, N, dtype=metric.dtype, device=metric.device)
+        dst_score=sim_beta*dst_score+noise_alpha*noise_score
+        rand_idx = find_patch_max_indices(dst_score, st, sx, sy, t, w, h)  # [B,hsy,wsx,1]
+        # Align CFG
+        # rand_idx = duplicate_half_tensor(rand_idx)
+        ##############################################################
+        # Method 2：LocalRandom
+        # generator = init_generator(metric.device)
+        # rand_idx = torch.randint(sy * sx, size=(hsy, wsx, 1), device=generator.device, generator=generator).to(metric.device)  # [hsy, wsx, 1]
+        # rand_idx = rand_idx.unsqueeze(0).expand(B, -1, -1, -1)
+        #############################################################
+        # Method3: Fixed selection of the top-left corner within each patch.
+        # rand_idx = torch.zeros(hsy, wsx, 1, device=metric.device, dtype=torch.int64)
+        # rand_idx = rand_idx.unsqueeze(0).expand(B,-1,-1,-1)
+        ##############################################################
+        idx_buffer_view = torch.zeros(B, hst, hsy, wsx, st * sy * sx, device=metric.device, dtype=torch.int64)
+        idx_buffer_view.scatter_(dim=-1, index=rand_idx, src=-torch.ones_like(rand_idx, dtype=rand_idx.dtype))
+        #idx_buffer_view = idx_buffer_view.view(B, hsy, wsx, sy, sx).transpose(2, 3).reshape(B, hsy * sy, wsx * sx)
+        idx_buffer_view = idx_buffer_view.view(B, hst, hsy, wsx, st, sy, sx).permute(0, 1, 4, 2, 5, 3, 6).reshape(B, hst * st, hsy * sy, wsx * sx)
+        if (hst * st) < t or (hsy * sy) < h or (wsx * sx) < w:
+            idx_buffer = torch.zeros(B, t, h, w, device=metric.device, dtype=torch.int64)
+            idx_buffer[:, :(hst * st), :(hsy * sy), :(wsx * sx)] = idx_buffer_view  # (B,h,w)
+        else:
+            idx_buffer = idx_buffer_view  # (B,h,w)
+        rand_idx = idx_buffer.reshape(B, -1).argsort(dim=1)  # (B,N)
+        del idx_buffer, idx_buffer_view
+
+        #############################################################
+        # Method 4: Randomly select within the global scope.
+        # random_permutation = torch.randperm(N).to(metric.device)
+        # rand_idx=random_permutation.unsqueeze(0).expand(B,-1)
+        ############################################################
+        # Method 5:  Select the maximum SimScore within the global scope.
+        # dst_score = consine_graph.sum(-1) # (B,N)
+        # _, rand_idx = torch.sort(dst_score, dim=1, descending=True)
+        #############################################################
+        num_dst = hst * hsy * wsx
+        a_idx = rand_idx[:, num_dst:]  # src  (B,N-num_dst) 0~N
+        b_idx = rand_idx[:, :num_dst]  # dst  (B,num_dst) 0~N
+
+        score_sim_1 = gather(consine_graph, dim=1, index=a_idx.unsqueeze(-1).expand(B, -1, N))  # (B,N-num_dst,N)
+        score_sim = gather(score_sim_1, dim=-1,
+                           index=b_idx.unsqueeze(1).expand(B, score_sim_1.shape[1], -1))  # (B,N-num_dst,num_dst)
+        score_sim_value, score_sim_index = torch.max(score_sim, dim=2)  # (B,N-num_dst) 0~num_dst
+        total_score = - score_sim_value
+
+        edge_idx = total_score.argsort(dim=-1)  # The size is (B, N-num_dst), sorted in ascending order, with values as indices (0~N-num_dst).
+        unm_idx = edge_idx[..., num_prune:]  # Size: (B,N-num_dst-num_prune) Value:（0~N-num_dst） [8,1024]  0~3071
+        src_idx = edge_idx[..., :num_prune]  # Size: (B,num_prune) Value:（0~N-num_dst）           [8,2048]  0~3071    1896
+        a_idx_tmp = a_idx.expand(B, N - num_dst)  # (8,3072) 0~4095
+        a_unm_idx = gather(a_idx_tmp, dim=1, index=unm_idx)  # Size: (B,N-num_dst-unm_prop) Value:（0~N）
+        a_src_idx = gather(a_idx_tmp, dim=1,
+                           index=src_idx)  # Size:(B,num_prune) Value（0~N）  idx 1896 out of bound with size 1024
+        
+        combined = torch.cat((a_unm_idx, b_idx), dim=1)  # (B,N-num_prune)
+        weight = gather(consine_graph, dim=1, index=combined.unsqueeze(-1).expand(B, -1, N))  # (B,N-num_prune,N)
+        weight_prop = gather(weight, dim=2,
+                             index=a_src_idx.unsqueeze(1).expand(-1, weight.shape[1], -1))  # (B,N-prune,num_prune)
+        _, max_indices = torch.max(weight_prop, dim=1)  # (B,num_prune) 0~N-num_prop
+
+    @torch.compiler.disable
+    def prune_tokens(x: torch.Tensor) -> torch.Tensor:  # x: (B,N,C)
+        B, N, C = x.shape  #
+        unm = gather(x, dim=-2,
+                     index=a_unm_idx.unsqueeze(2).expand(B, N - num_dst - num_prune, C))  # (B, N-num_dst-num_prune, C)
+        dst = gather(x, dim=-2, index=b_idx.unsqueeze(2).expand(B, num_dst, C))  # (B,num_dst,C)
+        result = torch.cat([unm, dst], dim=1)
+        return result  # (B,N-num_prune,c)
+
+    @torch.compiler.disable
+    def recover_tokens(x: torch.Tensor) -> torch.Tensor:  # (B,N-num_prune,c)
+        unm_len = a_unm_idx.shape[1]  # N-num_dst-num_prune
+        # unm: (B, N-num_dst-num_prune,C) dst: (B,num_dst,C)
+        unm, dst = x[..., :unm_len, :], x[..., unm_len:, :]
+        _, _, c = unm.shape
+        # weight_prop: (B,num_prune,num_dst)
+        # dst: (B,num_dst,C)
+        out = torch.zeros(B, N, c, device=x.device, dtype=x.dtype)
+        src = torch.gather(x, 1, max_indices.unsqueeze(-1).expand(-1, -1, c))  # (B,num_prune,c)
+        out.scatter_(dim=-2, index=b_idx.unsqueeze(2).expand(B, num_dst, c), src=dst)
+        out.scatter_(dim=-2, index=a_unm_idx.unsqueeze(2).expand(B, unm_len, c), src=unm)
+        out.scatter_(dim=-2, index=a_src_idx.unsqueeze(2).expand(B, num_prune, c), src=src)
+        return out  # (B,N,C)
+
+    return prune_tokens, recover_tokens
+
+def select_sito_method(x, grid_sizes): #, timestep):
+    
+    args = dict(prune_ratio = 0.5,
+        max_downsample_ratio = 1,
+        st = 1, sx = 2, sy = 2,
+        noise_alpha= 0.1,
+        sim_beta = 1)
+    
+    current_timestep = 6934 # unaffected
+    original_t, original_h, original_w = tuple(grid_sizes[0].cpu())  # 64,64
+    original_tokens = original_t * original_h * original_w
+    downsaple_ratio = int(math.ceil(math.sqrt(original_tokens // x.shape[1])))
+
+    if (downsaple_ratio <= args["max_downsample_ratio"]):
+        t = int(math.ceil(original_t / downsaple_ratio))  
+        w = int(math.ceil(original_w / downsaple_ratio))
+        h = int(math.ceil(original_h / downsaple_ratio))
+        num_prune = int(x.shape[1] * args["prune_ratio"])
+        p, r = prune_and_recover_tokens(x, num_prune, t=t, w=w, h=h, st=int(args['st']), sx=int(args['sx']), sy=int(args['sy']),noise_alpha=args['noise_alpha'],sim_beta=args['sim_beta'],current_timestep=current_timestep)
+    else:
+        p = do_nothing_s
+        r = do_nothing_s
+
+    return p, r
 
 class WanAttentionBlock(nn.Module):
 
@@ -700,6 +864,10 @@ class WanAttentionBlock(nn.Module):
 
         # self-attention
         x_ref_attn_map = None
+        
+        p_a, r_a = select_sito_method(x, grid_sizes)
+        
+        input_x = p_a(input_x)
 
         #query, key, value
         q, k, v = self.self_attn.qkv_fn(input_x)
@@ -750,6 +918,8 @@ class WanAttentionBlock(nn.Module):
                 y = self.self_attn.forward(q, k, v, seq_lens, attention_mode_override="sageattn")
         else:
             y = self.self_attn.forward(q, k, v, seq_lens)
+
+        y = r_a(y)
 
         # FETA
         if enhance_enabled:
